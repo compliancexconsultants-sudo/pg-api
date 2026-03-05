70164418307ae87565cf2b364bb8b8208ab8cf7a
chandu kanth <kruthichandu23@gmail.com>
2026-03-05 13:07:22 +0530

    new

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
 app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 // Routes
 app.get("/", (req, res) => {
