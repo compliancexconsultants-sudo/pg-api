4ca797667a83df063c789056315ba5cbc7acd267
chandu kanth <kruthichandu23@gmail.com>
2026-03-05 13:15:56 +0530

    neww

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,11 +1,17 @@
 const nodemailer = require("nodemailer");
 
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: false,
+  port: 587,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const sendEmail = async (to, subject, html) => {
